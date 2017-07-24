070a31307cdc2a84b3086ec46b3860d92a4d9a21
deshevan <deshevan@DESHEVAN>
2017-07-24 16:39:51 +0530

    Deleted .vs

---
+++ ASPNETWebApplication/Services/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETWebApplication.Services
+{
+    public class Constants
+    {
+        public static string HOSTNAME = "http://localhost:";
+    }
+}
