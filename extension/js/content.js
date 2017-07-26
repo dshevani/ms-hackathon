7f05ec788d125c59e93dc2be38c2d828ba049714
deshevan <deshevan@DESHEVAN>
2017-07-26 14:25:01 +0530

    Hackathon ready

--- extension/js/content.js
+++ extension/js/content.js
@@ -2,6 +2,18 @@ var greeting = $(".greeting-headline").text().trim();
 parts = greeting.split(",");
 name = parts[0];
 
+function delay(ms) {
+    var cur_d = new Date();
+    var cur_ticks = cur_d.getTime();
+    var ms_passed = 0;
+    while (ms_passed < ms) {
+        var d = new Date();  // Possible memory leak?
+        var ticks = d.getTime();
+        ms_passed = ticks - cur_ticks;
+        d = null;  // Prevent memory leak?
+    }
+}
+
 if (window.location.href.indexOf("premium") > -1) {
 
     fillDetais = "\
@@ -61,12 +73,37 @@ if (window.location.href.indexOf("premium") > -1) {
 } else if (window.location.href.indexOf("payments") > -1) {
 
     $(".product-icon h1").html("Smart Career Manager");
-    $(".product-info p").html(name + ", we have created a plan to achieve your goal");
+    $(".product-info p").html("Welcome, we have created a plan to achieve your goal");
     // Step 1 : Suggest courses
     span = $(".step-header:eq(0) h2").html();
     span = span.replace("Confirm your billing cycle", "Consider enrolling for these courses");
     $(".step-header:eq(0) h2").html(span);
-
+    $(".cart-chooser-container dl:eq(0) dt").html("<a href=\"https://www.linkedin.com/learning/jeff-weiner-on-establishing-a-culture-and-a-plan-for-scaling\">Establishing a Culture : Jeff Weiner</a>");
+    $(".cart-chooser-container dl:eq(1) dt").html("<a href=\"https://www.linkedin.com/learning/strategic-negotiation\">Strategic Negotiation</a>");
+    $(".footnote-text").text("We recommend you take above courses and take the first step. Meanwhile, we will connect you with experts and mentors.");
+    // Step 2 : Suggest mentors
+    span = $(".step-header:eq(1) h2").html();
+    span = span.replace("Payment", "Get guidance from expert mentors");
+    $(".step-header:eq(1) h2").html(span);
+    recommendations = "\
+            <table>\
+                <tr>\
+                  <td><img style=\"width:130px; height:140px;border-radius:35px;\"  src=\"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAxNAAAAJDZlYzAyYTZhLTNiMTQtNDVjNC04MTY4LTY0N2E5ZjM3NThhYw.jpg\" class=\"avatar member EntityPhoto-circle-3\"></td>\
+                  <td>&nbsp; &nbsp;</td>\
+                  <td><span class=\"feed-s-follows-module-recommendation__name Sans-15px-black-85%-semibold\"><a href=\"https://www.linkedin.com/in/shreyas-ganesh-n-2569695\">Shreyas Ganesh N</a></span></td>\
+                  <td>&nbsp; &nbsp;</td>\
+                  <td><button id=\"place-order\" type=\"button\" data-tracking-control-name=\"place_order\">Request Mentorship</button></td>\
+                </tr>\
+                <tr>\
+                  <td><img style=\"width:130px; height:140px;border-radius:35px;\"  src=\"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/000/068/0d2/067e2f6.jpg\" class=\"avatar member EntityPhoto-circle-3\"></td>\
+                  <td>&nbsp; &nbsp;</td>\
+                  <td><span class=\"feed-s-follows-module-recommendation__name Sans-15px-black-85%-semibold\"><a href=\"https://www.linkedin.com/in/kbangalore\">Kiran Bangalore</a></span></td>\
+                  <td>&nbsp; &nbsp;</td>\
+                  <td><button id=\"place-order\" type=\"button\" data-tracking-control-name=\"place_order\">Request Mentorship</button></td>\
+                </tr>\
+            </table>\
+    ";
+    $(".payment-method-display").html(recommendations);
 }
 
 else {
@@ -78,12 +115,30 @@ else {
         </div> \
         ";
 
-    $(popUpModal).insertAfter(".pe-hub-section:eq(0)");
+    
+    if ($(".pe-hub-section:eq(0)").length > 0) {
+        console.log("Adding module on user page");
+        $(popUpModal).insertAfter(".pe-hub-section:eq(0)");
+    }
+    feedUpdated = false;
+    if ($(".feed-right-rail").length > 0) {
+        console.log("Adding module on feed page");
+        $(popUpModal).insertBefore(".feed-right-rail .nav-footer");
+        feedUpdated = true;
+    }
+    if ($(".feed-right-rail").length > 0 && !feedUpdated) {
+        console.log("Adding module on feed page");
+        $(".right-rail-container .feed-s-follows-module").html(popUpModal);
+        feedUpdated = true;
+    }
+    if (feedUpdated) {
+        console.log("Added module on feed page");
+    }
 }
 
 
 chrome.runtime.sendMessage({ type: 'get_recommendations' }, function (response) {
-    console.log("get_recommendations");
+    console.log("Sending message get_recommendations");
 });
 
 chrome.runtime.onMessage.addListener(
@@ -93,18 +148,6 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
-
-function delay(ms) {
-    var cur_d = new Date();
-    var cur_ticks = cur_d.getTime();
-    var ms_passed = 0;
-    while (ms_passed < ms) {
-        var d = new Date();  // Possible memory leak?
-        var ticks = d.getTime();
-        ms_passed = ticks - cur_ticks;
-        d = null;  // Prevent memory leak?
-    }
-}
 
 
 
